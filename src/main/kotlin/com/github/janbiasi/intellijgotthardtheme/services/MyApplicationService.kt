04a422b84156e564be263096c3a395edb9368c03
GitHub Action <action@github.com>
2022-12-22 13:11:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/janbiasi/intellijgotthardtheme/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/janbiasi/intellijgotthardtheme/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.janbiasi.intellijgotthardtheme.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.janbiasi.intellijgotthardtheme.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/janbiasi/intellijgotthardtheme/services/MyProjectService.kt
+++ src/main/kotlin/com/github/janbiasi/intellijgotthardtheme/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.janbiasi.intellijgotthardtheme.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.janbiasi.intellijgotthardtheme.MyBundle
 
 class MyProjectService(project: Project) {
 
